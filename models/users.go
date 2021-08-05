f143f62fffdafbca5517d290e7419b1138acfe2b
ian <muhia>
2021-08-05 09:14:25 +0300

    created the userService for interacting with user models

--- models/users.go
+++ models/users.go
@@ -1,9 +1,70 @@
 package models
 
 import (
-	_ "gorm.io/driver/postgres"
+	"errors"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm/logger"
+
 	"gorm.io/gorm"
 )
+
+var (
+	/**
+	 * ErrNotFound is returned when a reasouce is not found
+	 */
+	ErrNotFound = errors.New("models: reasource not found")
+)
+
+func NewUserService(connectionInfo string) (*UserService, error) {
+
+	// dsn := "host=localhost user=postgres password=postgres dbname=lenslocked_dev port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(connectionInfo), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		return nil, err
+	}
+	return &UserService{
+		db: db,
+	}, nil
+
+}
+
+type UserService struct {
+	db *gorm.DB
+}
+
+/**ByID will lookup a user by the id provided
+* 1 - user, nil
+* 2 - nil, ErrNotFound
+* 3 - nil, otherError
+ */
+func (us *UserService) ByID(id uint) (*User, error) {
+	var user User
+	err := us.db.Where("id = ?", id).First(&user).Error
+	switch err {
+	case nil:
+		return &user, nil
+	case gorm.ErrRecordNotFound:
+		return nil, ErrNotFound
+	default:
+		return nil, err
+
+	}
+
+}
+
+func (us *UserService) DestructiveReset() {
+	us.db.Migrator().DropTable(&User{})
+	us.db.AutoMigrate(&User{})
+}
+
+func (us *UserService) Close(error) {
+	db, _ := us.db.DB()
+	db.Close()
+
+}
 
 type User struct {
 	gorm.Model
